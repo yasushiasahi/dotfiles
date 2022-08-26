483a8b0dd8ed9054cfcd33a977423a46ca3de18a
zero_asahi <zero_asahi@MacBook-Air.local>
2022-08-26 19:20:47 +0900

    f

---
+++ emacs/etc/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(corfu-doc corfu marginalia orderless consult vertico multiple-cursors which-key vundo rainbow-delimiters expand-region smartparens tree-sitter-langs tree-sitter fira-code-mode solarized-theme macrostep leaf-convert leaf-tree hydra leaf-keywords leaf)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
